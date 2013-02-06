e4307e7f67389b9862411c07a8afe3d2b078b477
Jonny Ramos <jramos@uchicago.edu>
2013-02-06 13:57:02 -0600

    Initial Commit

---
+++ TwitterClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterClient
+//
+//  Created by JRamos on 2/6/13.
+//  Copyright (c) 2013 JRamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
